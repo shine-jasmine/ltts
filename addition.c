6d49f7250b930ae54f324f9c7a2d0a7e992f7d7d
shine-jasmine <80335764+shine-jasmine@users.noreply.github.com>
2021-03-25 19:29:37 +0530

    Delete addition.c

--- addition.c
+++
@@ -1,9 +0,0 @@
-
-#include "myfunc.h"
-
-complex_t sum(complex_t num1, complex_t num2){
-    complex_t sum;
-    sum.real = (num1.real + num2.real);
-    sum.imaginary = (num1.imaginary + num2.imaginary);
-    return sum;
-}
